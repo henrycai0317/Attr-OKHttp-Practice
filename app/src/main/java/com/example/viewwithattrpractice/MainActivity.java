63fb0834cb00ba0d84deddfc9973ce7fd8c88863
Henry <henry.cai@shoalter.com>
2021-12-15 13:32:15 +0800

    OKHttp request & call implement

--- app/src/main/java/com/example/viewwithattrpractice/MainActivity.java
+++ app/src/main/java/com/example/viewwithattrpractice/MainActivity.java
@@ -1,14 +1,52 @@
 package com.example.viewwithattrpractice;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    //建立OKHttpClient
+    OkHttpClient client = new OkHttpClient().newBuilder().build();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //建立Request，設置動物園館資料連線
+        Request request = new Request.Builder()
+                .url("https://jsonplaceholder.typicode.com/posts/1")
+                .build();
+
+        //建立Call
+        Call call = client.newCall(request);
+        //執行Call連線
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                //連線失敗
+                Log.d(TAG, "onFailure: " + e.getMessage());
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                //連線成功，從Response取得連線結果
+                String result = response.body().string();
+                Log.d(TAG, "onResponse: "+result);
+            }
+        });
     }
 }
